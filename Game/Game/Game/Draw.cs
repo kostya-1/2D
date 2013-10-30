333ec1e08cc4e1c01de20af13026110dce33275c
kostya-1 <try1.zipzap@gmail.com>
2013-10-30 14:02:06 +0000

    Writed the class Draw
    used spriteBatch from Game1

---
+++ Game/Game/Game/Draw.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Game
+{
+    class Draw
+    {
+        #region data
+        Texture2D texture;
+        Vector2 position;
+        Rectangle? rectungle;
+        Color color;
+        float rotation;
+        Vector2 origin;
+        Vector2 scale;
+        SpriteEffects effects;
+        float layer;
+        #endregion
+
+        #region constructor
+
+        public Draw(Texture2D texture, Vector2 position, Rectangle? sourceRectangle, Color color, float rotation, Vector2 origin, Vector2 scale, SpriteEffects effects, float layerDepth)
+        {
+            this.texture = texture;
+            this.position = position;
+            this.rectungle = sourceRectangle;
+            this.color = color;
+            this.rotation = rotation;
+            this.origin = origin;
+            this.scale = scale;
+            this.effects = effects;
+            this.layer = layerDepth;
+        }
+
+        #endregion
+
+        public void draw()
+        {
+            Game1.spriteBatch.Draw(texture, position, rectungle, color, rotation, origin, scale, effects, layer);
+        }
+    }
+}
